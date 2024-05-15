06db0d951b2922fcdf827fc0cf7bec1a4f471dbd
JohnAzizJA <johnazizja10@gmail.com>
2024-05-15 16:42:10 +0300

    linked timeline controller

--- src/main/java/com/example/saisoof/TimelineController.java
+++ src/main/java/com/example/saisoof/TimelineController.java
@@ -1,5 +1,46 @@
 package com.example.saisoof;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TimelineController {
 
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private Hyperlink profileLink;
+
+    @FXML
+    private Hyperlink addPostLink;
+
+    @FXML
+    private void handleProfile() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("ProfilePage.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.setTitle("HIVE");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            Stage currentStage = (Stage) profileLink.getScene().getWindow();
+            currentStage.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleAddPost() {
+
+    }
 }
