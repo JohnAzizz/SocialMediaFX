5a6048fab978d13d8ef711b5166d5063ccdc0345
JohnAzizJA <johnazizja10@gmail.com>
2024-05-16 03:19:35 +0300

    following center aligned

--- src/main/java/com/example/saisoof/FollowersController.java
+++ src/main/java/com/example/saisoof/FollowersController.java
@@ -18,29 +18,20 @@ public class FollowersController implements Initializable {
     @FXML
     private ScrollPane scrollFollowing;
 
-    VBox allFollowing = new VBox(10);
+    VBox allFollowing = new VBox(15);
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         for (UserProfile following : Login.getCurrentUser().getFollowing()){
-            ImageView profilePic = new ImageView();
-            if (following.getProfilePicture() != null){
-                profilePic.setImage(new Image(following.getProfilePicture()));
-                profilePic.setFitWidth(75);
-                profilePic.setFitHeight(75);
-            }
-
-
             Label usernameLabel = new Label(following.getUsername());
             usernameLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
 
-            HBox details = new HBox(20, profilePic, usernameLabel);
-            details.setAlignment(Pos.CENTER);
-
-            allFollowing.getChildren().add(details);
+            allFollowing.getChildren().add(usernameLabel);
+            allFollowing.setAlignment(Pos.CENTER);
 
         }
 
         scrollFollowing.setContent(allFollowing);
+        scrollFollowing.setFitToWidth(true);
     }
 }
